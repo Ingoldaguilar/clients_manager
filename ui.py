05caf377c0845dd9b8cdaa8e490a6e4c377b9965
ingold <ingoldaguilar45@gmail.com>
2023-03-07 18:42:10 -0600

    Create a function for center the ui window
    - Create a center function in ui.py for center the window.
    - Create a center widget mixin for center the window.

--- ui.py
+++ ui.py
@@ -8,12 +8,28 @@ A file for store the user interface.
 from tkinter import *
 # ---- End Imports ----
 
-class MainWindow(Tk):
+class CenterWidgetMixin:
+
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int(ws / 2 - w / 2)
+        y = int(hs / 2 - h / 2)
+
+        # how it works geometry("WIDTHxHEIGHT+OFFSET_X+OFFSET_Y")
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+
+class MainWindow(Tk, CenterWidgetMixin):
 
     def __init__(self):
         super().__init__()
         self.title("Clients Manager")
         self.build()
+        self.center()
 
     def build(self):
         button = Button(self, text="Hello", command=self.hello)
@@ -21,6 +37,19 @@ class MainWindow(Tk):
 
     def hello(self):
         print("Hello world!")
+        self.destroy()
+
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int(ws/2 - w/2)
+        y = int(hs/2 - h/2)
+
+        # how it works geometry("WIDTHxHEIGHT+OFFSET_X+OFFSET_Y")
+        self.geometry(f"{w}x{h}+{x}+{y}")
 
 if __name__ == "__main__":
     app = MainWindow()
