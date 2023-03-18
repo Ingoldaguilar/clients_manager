0f627a889f4a17ed59dd17739e7056386bf45bf2
ingold <ingoldaguilar45@gmail.com>
2023-03-17 18:43:05 -0600

    Add terminal menu if insert '-t' at the end of the run.py. Note: End of the project

--- run.py
+++ run.py
@@ -6,6 +6,12 @@ Main file of the program.
 
 # ---- Imports ----
 import menu
+import sys
+import ui
 # ---- End Imports ----
 if __name__ == "__main__":
-    menu.init()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.init()
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
